a990ecbadf0c108c0243b8ae6e0233526037560c
Ryan <griffinryan77@gmail.com>
2024-01-24 17:02:31 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string ){
+    const names = drivers.filter( driverName => driverName.toLowerCase() === string.toLowerCase())
+    // if (string === "Sammy" ){
+    //     return ["Sammy"]
+    // }
+    return names 
+}
+
+function fuzzyMatch (drivers, string){
+    const names = drivers.filter(driverName => driverName.slice(0, 2)=== string)
+    return names
+}
+
+function matchName(drivers, string){
+    const names = drivers.filter(driverObject => driverObject.name === string )
+    return names
+}
